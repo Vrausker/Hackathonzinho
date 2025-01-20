103f2947d226c205b55c6db7b56ba7221e5c31c6
Vrausker <gabrielbdcoelho@gmail.com>
2025-01-20 11:42:51 -0300

    [FEAT] [HOME] [BENEFICIOS/FOOTER]

--- locadora-filmes-jogos/src/Pages/Home.jsx
+++ locadora-filmes-jogos/src/Pages/Home.jsx
@@ -3,27 +3,27 @@ import "../Styles/Home.css";
 import { useEffect } from "react";
 
 const HomePage = () => {
-    useEffect(() => {
-      const swiper = new Swiper(".swiper", {
-        direction: "horizontal",
-        slidesPerView: 4,
-        loop: true,
-        spaceBetween: 0,
-        // autoplay: {
-        //     delay: 2500,               
-        //     disableOnInteraction: false,
-        //   },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        breakpoints: {
-          320: { slidesPerView: 1 },
-          768: { slidesPerView: 2 },
-          1024: { slidesPerView: 4 },
-        },
-      });
-    },);
+  useEffect(() => {
+    const swiper = new Swiper(".swiper", {
+      direction: "horizontal",
+      slidesPerView: 4,
+      loop: true,
+      spaceBetween: 0,
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        320: { slidesPerView: 1 },
+        768: { slidesPerView: 2 },
+        1024: { slidesPerView: 4 },
+      },
+    });
+  });
 
   return (
     <div>
@@ -47,64 +47,96 @@ const HomePage = () => {
         </div>
       </section>
       <section>
-        <div className="titleFilmes"> 
-            <h1> FILMES EM ALTA! </h1>
+        <div className="titleFilmes">
+          <h1> FILMES EM ALTA! </h1>
         </div>
         <div className="swiper">
           <div className="swiper-wrapper">
-                <div className="swiper-slide">
-                  <img src="src\assets\Filme_Alta_01.png" />
-                </div>
-                <div className="swiper-slide">
-                  <img src="src\assets\Filme_Alta_02.png" />
-                </div>
-                <div className="swiper-slide">
-                  <img src="src\assets\Filme_Alta_03.png" />
-                </div>
-                <div className="swiper-slide">
-                  <img src="src\assets\Filme_Alta_04.png" />
-                </div>
-                <div className="swiper-slide">
-                  <img src="src\assets\Filme_Alta_05.png" />
-                </div>
+            <div className="swiper-slide">
+              <img src="src\assets\Filme_Alta_01.png" />
+            </div>
+            <div className="swiper-slide">
+              <img src="src\assets\Filme_Alta_02.png" />
+            </div>
+            <div className="swiper-slide">
+              <img src="src\assets\Filme_Alta_03.png" />
+            </div>
+            <div className="swiper-slide">
+              <img src="src\assets\Filme_Alta_04.png" />
+            </div>
+            <div className="swiper-slide">
+              <img src="src\assets\Filme_Alta_05.png" />
+            </div>
           </div>
           <div className="swiper-button-prev"></div>
-          <div className="swiper-button-next">
+          <div className="swiper-button-next"></div>
+        </div>
+      </section>
+      <section>
+        <div className="titleFilmes">
+          <h1> GAMES EM ALTA! </h1>
+        </div>
+        <div className="swiper">
+          <div className="swiper-wrapper">
+            <div className="swiper-slide">
+              <img src="src\assets\Game_Alta_01.png" />
+            </div>
+            <div className="swiper-slide">
+              <img src="src\assets\Game_Alta_02.png" />
+            </div>
+            <div className="swiper-slide">
+              <img src="src\assets\Game_Alta_03.png" />
+            </div>
+            <div className="swiper-slide">
+              <img src="src\assets\Game_Alta_04.png" />
+            </div>
+            <div className="swiper-slide">
+              <img src="src\assets\Game_Alta_05.png" />
+            </div>
+          </div>
+          <div className="swiper-button-prev"></div>
+          <div className="swiper-button-next"></div>
+        </div>
+      </section>
+      <section>
+        <div className="titleBeneficios">
+          <h1> POR QUE LOCAR? </h1>
+        </div>
+        <div className="beneficios">
+          <div className="beneficiosList">
+            <h2> NÃO PRECISA DE ASSINATURA </h2>
+            <div className="rectangle"></div>
+            <p>
+              Assinar virou coisa do passado, agora você tem a oportunidade
+              locar somente quando você quiser consumir o produto
+            </p>
+          </div>
+          <div className="beneficiosList">
+            <h2> PREÇOS ACESSÍVEIS </h2>
+            <div className="rectangle"></div>
+            <p>
+              Valores que valem a pena para o seu bolso e sem taxa de adesão,
+              você só paga pelo que aluga.
+            </p>
+          </div>
+          <div className="beneficiosList">
+            <h2> ENTREGA RÁPIDA </h2>
+            <div className="rectangle"></div>
+            <p>
+              Ao realizar o pagamento, você já consegue o acesso imediato ao
+              filme ou game que alugou.
+            </p>
           </div>
         </div>
       </section>
       <section>
-      <div className="titleFilmes"> 
-            <h1> GAMES EM ALTA! </h1>
+        <div className="footer">
+        <img src="src\assets\marca.png"/>
+          <p> Filmes </p>
+          <p> Games </p>
+          <p> Política de Privacidade </p>
+          <p> Termos de uso </p>
         </div>
-        <div className="swiper">
-          <div className="swiper-wrapper">
-                <div className="swiper-slide">
-                  <img src="src\assets\Game_Alta_01.png" />
-                </div>
-                <div className="swiper-slide">
-                  <img src="src\assets\Game_Alta_02.png" />
-                </div>
-                <div className="swiper-slide">
-                  <img src="src\assets\Game_Alta_03.png" />
-                </div>
-                <div className="swiper-slide">
-                  <img src="src\assets\Game_Alta_04.png" />
-                </div>
-                <div className="swiper-slide">
-                  <img src="src\assets\Game_Alta_05.png" />
-                </div>
-          </div>
-          <div className="swiper-button-prev"></div>
-          <div className="swiper-button-next">
-          </div>
-        </div>
-    </section>
-      <section>
-        <div>
-            <h1> POR QUE LOCAR? </h1>
-        </div>
-        
       </section>
     </div>
   );
