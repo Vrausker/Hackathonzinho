ac6f12378222d013d692a3fe0288980613b6a583
Vrausker <gabrielbdcoelho@gmail.com>
2025-01-27 10:01:57 -0300

    [FEAT] [HOME] [MENU RESPONSIVO]

--- locadora-filmes-jogos/src/Components/Menu.jsx
+++ locadora-filmes-jogos/src/Components/Menu.jsx
@@ -1,16 +1,41 @@
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import "../Styles/Menu.css";
 
 const Menu = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div>
       <nav>
-        <img src="src\assets\marca.png" alt="logo-locaflick"/>
-        <Link to="/"> Início </Link>
-        <Link to="/filmes"> Filmes </Link>
-        <Link to="/games"> Games </Link>
-        <Link to="/politica"> Política de Privacidade </Link>
-        <Link to="/login"> Login | Cadastre </Link>
+        <img src="src\assets\marca.png" alt="logo-locaflick" />
+        <div
+          className="menu"
+          onClick={() => {
+            setMenuOpen(!menuOpen);
+            console.log("Boolean_Funciona", !menuOpen);
+          }}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <ul className={menuOpen ? "open" : ""}>
+          <li>
+            <NavLink to="/"> Início </NavLink>
+          </li>
+          <li>
+            <NavLink to="/filmes"> Filmes </NavLink>
+          </li>
+          <li>
+            <NavLink to="/games"> Games </NavLink>
+          </li>
+          <li>
+            <NavLink to="/politica"> Política de Privacidade </NavLink>
+          </li>
+          <li>
+            <NavLink to="/login"> Login | Cadastre </NavLink>
+          </li>
+        </ul>
       </nav>
     </div>
   );
