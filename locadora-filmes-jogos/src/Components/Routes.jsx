af177c098f6804932745f9dfec14d17923df7bd1
Vrausker <gabrielbdcoelho@gmail.com>
2025-01-30 12:38:52 -0300

    [FEAT] [PAG LOGIN CADASTRO]

--- locadora-filmes-jogos/src/Components/Routes.jsx
+++ locadora-filmes-jogos/src/Components/Routes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../Pages/Home";
+import CadLoginPage from "../Pages/Cadastro/CadLogin";
 
 const AppRoutes = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<HomePage/>}/>
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<CadLoginPage />} />
+    </Routes>
+  );
 };
 
 export default AppRoutes;
---
+++ locadora-filmes-jogos/src/Pages/Cadastro/CadLogin.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "../../Styles/CadLogin.css";
+
+const CadLoginPage = () => {
+  const [login, setLogin] = useState("");
+  const [pass, setPass] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Login:", login);
+    console.log("Senha:", pass);
+  };
+
+  return (
+    <div className="loginContainer">
+      <div>
+        <label>
+          Login
+          <input
+            type="text"
+            placeholder="Insira seu Login"
+            name="login"
+            value={login}
+            onChange={(e) => setLogin(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Senha
+          <input
+            type="password"
+            placeholder="Insira sua Senha"
+            name="password"
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+          />
+        </label>
+        <p className="register"> Não tem conta? Registre-se! </p>
+      </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CadLoginPage;
